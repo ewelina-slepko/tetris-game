9f628c41558ded3eaa16cfebd1ac21d932a5ece6
pinguinNerd <ewelina.slepko1@gmail.com>
2019-03-19 23:58:18 +0100

    New changes
    It has been modified Draw class and added new function controlled fps.

--- tetris.js
+++ tetris.js
@@ -19,16 +19,18 @@ class Square {
         this.board.context.fillStyle = this.color;
         this.board.context.fillRect(this.xPosition * sqrSize, this.yPosition * sqrSize, sqrSize, sqrSize);
     }
-    removeSqr(obj) {
-        const sqrSize = 30;
-        this.board.context.fillStyle = ' #006064';
-        this.board.context.fillRect(this.xPosition * sqrSize, this.yPosition * sqrSize - sqrSize, sqrSize, sqrSize);
+    removeSqr() {
+        this.board.context.clearRect(0, 0, 300, 600);
     }
     moveDown() {
         this.yPosition++;
         this.board.gameBoard[this.yPosition];
+        //+ przypisać jedynkę w pozycji na tablicy.
+        //console.log(this.board.gameBoard[this.yPosition]);
+
+        //A z tą osobna klasą, to mi chodziło o te metody, żeby była klasa, która reprezentuje shape i tam masz 
+        //moveDown i inne movy, a także inna klasa (np. Drawer), gdzie powinny metody drawBoard, czy clearBoard.
     }
-    dropDown = setInterval(this.moveDown.bind(this), 1000);
 }
 let activeSqr = new Square(activeBoard, 4, 0, 'yellow');
 
@@ -36,10 +38,25 @@ let activeSqr = new Square(activeBoard, 4, 0, 'yellow');
 class Draw {
     constructor(shape) {
         this.shape = shape;
+        this.dropCounter = 0;
+        this.lastTime = 0;
     }
-    update() {
+    update(time = 0) {
+        const deltaTime = time - this.lastTime;
+        this.lastTime = time;
+        this.dropCounter += deltaTime;
+
+        if (this.dropCounter > 1000) {
+            this.shape.moveDown();
+            this.dropCounter = 0;
+            if (this.shape.yPosition == 6) {
+                return;
+            }
+        }
+        this.shape.removeSqr();
         this.shape.drawSqr();
         requestAnimationFrame(this.update.bind(this));
     }
 }
-new Draw(activeSqr).update();
+let drawing = new Draw(activeSqr, activeBoard)
+drawing.update();

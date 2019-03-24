f524175af3fc78366d6334f127dec2849497dc69
pinguinNerd <ewelina.slepko1@gmail.com>
2019-03-24 23:45:00 +0100

    New background and button onClick

--- public/tetris.js
+++ public/tetris.js
@@ -110,6 +110,9 @@ class Game {
     constructor(shape) {
         this.shape = shape;
         window.addEventListener('keydown', this.onKeydown.bind(this));
+        document.getElementById('left').addEventListener('click', () => this.shape.moveLeft());
+        document.getElementById('down').addEventListener('click', () => this.shape.moveDown());
+        document.getElementById('right').addEventListener('click', () => this.shape.moveRight());
     }
     onKeydown(e) {
         switch (e.keyCode) {
